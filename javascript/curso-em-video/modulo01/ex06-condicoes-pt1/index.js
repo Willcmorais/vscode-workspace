24977abfd95b88b8707802edbfb5ebcf949cbccf
William coelho de morais <willcmorais@gmail.com>
2024-10-11 16:50:42 -0300

    commit-24-10-11-js-DETRAN

---
+++ javascript/curso-em-video/modulo01/ex06-condicoes-pt1/index.js
@@ -0,0 +1,9 @@
+//quando não temos else, chamamos de condição simples
+var vel = 60.5;
+console.log(`A velocidade atual é de ${vel}km por hora`);
+if (vel > 60) {
+  console.log("Você ultrapassou a velocidade permitida na via. MULTADO!");
+}
+
+//Em javascript para escrever algo na tela utilizamos este comando, console.log()
+console.log("Dirija sempre utilizando o cinto de segurança!");
